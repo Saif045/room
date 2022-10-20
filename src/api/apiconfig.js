d658ac9b2de4e099daf230ef890179b08964ae84
Saif045 <saif.x45@gmail.com>
2022-10-20 06:13:27 +0200

    fsa

--- src/api/apiconfig.js
+++ src/api/apiconfig.js
@@ -1,15 +1,5 @@
 
-export const options = {
-  method: "GET",
-  url: "https://kohls.p.rapidapi.com/categories/list",
-  params: {seoURL
-    : 
-    "/catalog/bookcases-shelving-furniture.jsp?CN=Category:Bookcases%20%26%20Shelving+Department:Furniture&cc=furniture-TN3.0-S-AccentFurniture-Bookcases"} ,
-  headers: {
-    "X-RapidAPI-Key": "2b8ee8c69amsh999c3ac61f8cbc5p191fcfjsn45ced037faa0",
-    "X-RapidAPI-Host": "kohls.p.rapidapi.com",
-  },
-};
+
 
 
 
