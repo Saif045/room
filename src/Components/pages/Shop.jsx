9559c801237d14a01c9f7473e64550a17f52e1de
Saif045 <saif.x45@gmail.com>
2022-10-20 06:05:06 +0200

    fg

--- src/Components/pages/Shop.jsx
+++ src/Components/pages/Shop.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { options } from "../../api/apiconfig";
+import axios from "axios";
 
 const Shop = () => {
-  return (
-    <div>
-      Shop</div>
-  )
-}
+  const [furniture, setfurniture] = useState([]);
 
-export default Shop
+  useEffect(() => {
+    const categoryReq = axios
+      .request(options)
+      .then(function (response) {
+        setfurniture(response.data.payload.categories[16]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    categoryReq;
+  }, []);
+
+
+  console.log(furniture);
+
+  return <div>Shop</div>;
+};
+
+export default Shop;
