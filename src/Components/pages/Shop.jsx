ed2954810c06f517351cf4f8d9c543ae62cd2a1d
Saif045 <saif.x45@gmail.com>
2022-10-26 03:33:42 +0200

    sf

--- src/Components/pages/Shop.jsx
+++ src/Components/pages/Shop.jsx
@@ -4,22 +4,31 @@ const Shop = () => {
   return (
     <div>
       <div className="h-20 bg-black block"></div>
-      <div className="m-10 text-center font-bold text-3xl">    Products 
-      <div className="grid grid-cols-3 gap-6  mt-6" >
-        {data.map((element, index) => {
-          const randomImage =
-            element.images[Math.floor(Math.random() * element.images.length)];
+      <div className=" m-2 sm:m-10 lg:mx-24 xl:mx-32 2xl:mx-44  text-center font-bold text-3xl">
+        {" "}
+        Products
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 sm:gap-6  mt-6 ">
+          {data.map((element, index) => {
+            const randomImage =
+              element.images[Math.floor(Math.random() * element.images.length)];
 
-          return (
-           
-              <div className=" flex flex-col-reverse bg-slate-300 " key={index}>
-              <p className=" text-xl font-semibold">{element.title}</p>
-              <img className="w-full h-full object-cover object-center " src={randomImage} />
-              
-            </div>
-          );
-        })}
-      </div>
+            return (
+              <div
+                className=" flex flex-col-reverse  bg-gradient-to-r  from-slate-100  "
+                key={index}>
+                <div className="bg-gradient-to-tr  from-slate-50">
+                  <p className=" text-left text-black text-base sm:text-lg lg:text-xl font-medium border-2 ">
+                    {element.title}
+                  </p>
+                </div>
+                <img
+                  className="w-full h-full object-cover object-center "
+                  src={randomImage}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
