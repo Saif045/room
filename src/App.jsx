bb983a4594a9c91b32f0ae94c11edc29b9d9b490
Saif045 <saif.x45@gmail.com>
2022-10-28 03:54:08 +0200

    sda

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import Header from "./Components/Header";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
 
       <Routes>
